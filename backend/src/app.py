27d02b91da48c759ce4c0b04d5faf5ffa745f036
alfredo <alfredo.supanta.26@gmail.com>
2022-02-19 11:14:04 -0300

    se crea rutas para la app en total 11

--- backend/src/app.py
+++ backend/src/app.py
@@ -10,6 +10,47 @@ CORS(app)
 def index():
     return render_template("index.html")
 
+@app.route('/api/people')
+def people():
+    return render_template("index.html")
+
+@app.route('/api/people/<int:people_id>')
+def single_people(people_id):
+    return render_template("index.html")
+
+@app.route('/api/planets')
+def planets():
+    return render_template("index.html")
+
+@app.route('/api/planets/<int:planet_id>')
+def single_planet(planet_id):
+    return render_template("index.html")
+
+@app.route('/api/users')
+def users():
+    return render_template("index.html")
+
+@app.route('/api/users/favorites')
+def favorites():
+    return render_template("index.html")
+
+@app.route('/api/favorite/planet/<int:planet_id>')
+def favorite_planet(planet_id):
+    return render_template("index.html")
+
+@app.route('/api/favorite/people/<int:planet_id>')
+def favorite_people(planet_id):
+    return render_template("index.html")
+
+@app.route('/api/favorite/planet/<int:planet_id>')
+def favorite_planet_deleted(planet_id):
+    return render_template("index.html")
+
+@app.route('/api/favorite/people/<int:people_id>')
+def favorite_people_deleted(people_id):
+    return render_template("index.html")
+
+
 
 if __name__ == '__main__':
     app.run()
